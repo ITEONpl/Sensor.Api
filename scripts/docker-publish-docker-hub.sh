7a9ed475cf497625d0a7e9196805e9dbc5aa3ed9
Leszek Szpunar <leszekszpunar@hotmail.com>
2018-07-02 23:19:31 +0200

    fix

--- scripts/docker-publish-docker-hub.sh
+++ scripts/docker-publish-docker-hub.sh
@@ -16,5 +16,5 @@ esac
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -f ./src/Sensor.Api/Dockerfile.$DOCKER_ENV -t sensor.api:$DOCKER_TAG ./src/Sensor.Api
-docker tag Sensor.api:$DOCKER_TAG $DOCKER_USERNAME/sensor.api:$DOCKER_TAG
+docker tag sensor.api:$DOCKER_TAG $DOCKER_USERNAME/sensor.api:$DOCKER_TAG
 docker push $DOCKER_USERNAME/sensor.api:$DOCKER_TAG
