62bc07984d00ee2f7c1524e30534574f961d941a
Leszek Szpunar <leszekszpunar@hotmail.com>
2018-07-03 22:53:25 +0200

    api

---
+++ src/Sensor.Api/Controllers/Api/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sensor.Api.Controllers
+{
+    [Route("[controller]")]
+    public class BaseController : Controller
+    {
+    }
+}
---
+++ src/Sensor.Api/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sensor.Api.Controllers
+{
+    [Route("")]
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json("Welcome in API");
+        }
+    }
+}
