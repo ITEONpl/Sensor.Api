c1aaf0e6a10927f47913deb81f3ccf90312677db
Leszek Szpunar <leszekszpunar@hotmail.com>
2018-07-03 23:36:41 +0200

    added nuget packages

--- src/Sensor.Api/Startup.cs
+++ src/Sensor.Api/Startup.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Reflection;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -8,29 +12,64 @@ namespace Sensor.Api
 {
     public class Startup
     {
+        private static readonly string[] Headers = {"X-Operation", "X-Resource", "X-Total-Count"};
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        public IContainer Container { get; private set; }
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", cors =>
+                    cors.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                        .WithExposedHeaders(Headers));
+            });
+
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyTypes(Assembly.GetEntryAssembly())
+                .AsImplementedInterfaces();
+            builder.Populate(services);
+            //builder.AddRabbitMq();
+
+
+            Container = builder.Build();
+
+            return new AutofacServiceProvider(Container);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            IApplicationLifetime applicationLifetime)
         {
-            if (env.IsDevelopment())
-                app.UseDeveloperExceptionPage();
-            else
-                app.UseHsts();
+            //if (env.IsDevelopment())
+            //    app.UseDeveloperExceptionPage();
+            //else
+            //    app.UseHsts();
 
+            if (env.IsDevelopment() || env.EnvironmentName == "local")
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("CorsPolicy");
             app.UseHttpsRedirection();
             app.UseMvc();
+            applicationLifetime.ApplicationStopped.Register(() => Container.Dispose());
         }
     }
 }
